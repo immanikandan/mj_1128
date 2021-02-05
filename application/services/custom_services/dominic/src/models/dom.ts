2f05b001af4848e6d2344f5faf42ec4a51684131
immanikandan <immanikandan>
2021-02-05 14:30:10 +0000

    updated generated code

--- application/services/custom_services/dominic/src/models/dom.ts
+++ application/services/custom_services/dominic/src/models/dom.ts
@@ -1,13 +1,5 @@
-
-import * as mongoose from 'mongoose';
-
-const Schema = mongoose.Schema;
-
-export const domSchema = new Schema({
-   titleid: String,
-   title: String,
-   description: String
-})
-
-const domModel = mongoose.model('dom', domSchema, 'dom');
-export default domModel;
+export class dom{
+    titleid: String;
+    title: String;
+    description: String;
+}
